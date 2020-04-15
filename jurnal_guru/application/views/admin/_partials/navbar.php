d68e06cabe615d2f6b6d05d5edc22c3729a76f7c
agung wahyu <agungwahyu23699@gmail.com>
2020-04-15 07:28:38 +0700

    membuat template

---
+++ jurnal_guru/application/views/admin/_partials/navbar.php
@@ -0,0 +1,64 @@
+<div class="sidebar-header">
+    <a href="index.html"><img class="main-logo" src="<?= base_url('assets/img/logo/logo.png') ?>" alt="" /></a>
+    <strong><img src="<?= base_url('assets/img/logo/logosn.png') ?>" alt="" /></strong>
+</div>
+<div class="nalika-profile">
+    <div class="profile-dtl">
+        <a href="#"><img src="<?= base_url('assets/img/notification/4.jpg') ?>" alt="" /></a>
+        <h2>Lakian <span class="min-dtn">Das</span></h2>
+    </div>
+</div>
+<div class="left-custom-menu-adp-wrap comment-scrollbar">
+    <nav class="sidebar-nav left-sidebar-menu-pro">
+        <ul class="metismenu">
+            <li class="active">
+                <a href="#">
+                        <i class="fa fa-home icon-wrap"></i>
+                        <span class="mini-click-non">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="mailbox.html" aria-expanded="false"><i class="fa fa-book icon-wrap"></i> <span class="mini-click-non">Data Mengajar</span></a>
+            </li>
+            <li>
+                <a href="mailbox.html" aria-expanded="false"><i class="fa fa-exchange icon-wrap"></i> <span class="mini-click-non">Permintaan Mengajar</span></a>
+            </li>
+            <li>
+                <a href="mailbox.html" aria-expanded="false"><i class="fa fa-money icon-wrap"></i> <span class="mini-click-non">Penarikan Saldo</span></a>
+            </li>
+            <li>
+                <a href="mailbox.html" aria-expanded="false"><i class="fa fa-envelope icon-wrap"></i> <span class="mini-click-non">Izin Guru</span></a>
+            </li>
+            <li>
+                <a href="mailbox.html" aria-expanded="false"><i class="fa fa-line-chart icon-wrap"></i> <span class="mini-click-non">Statistik Guru</span></a>
+            </li>
+            <center><h4>Data Master</h4></center>
+            <li>
+                <a href="mailbox.html" aria-expanded="false"><i class="icon nalika-forms icon-wrap"></i> <span class="mini-click-non">Tahun Ajaran</span></a>
+            </li>
+            <li>
+                <a href="mailbox.html" aria-expanded="false"><i class="icon nalika-forms icon-wrap"></i> <span class="mini-click-non">Data Jurusan</span></a>
+            </li>
+            <li>
+                <a class="has-arrow" href="mailbox.html" aria-expanded="false"><i class="icon nalika-smartphone-call icon-wrap"></i> <span class="mini-click-non">App views</span></a>
+                <ul class="submenu-angle" aria-expanded="false">
+                    <li><a title="Notifications" href="notifications.html"><span class="mini-sub-pro">Notifications</span></a></li>
+                    <li><a title="Alerts" href="alerts.html"><span class="mini-sub-pro">Alerts</span></a></li>
+                    <li><a title="Modals" href="modals.html"><span class="mini-sub-pro">Modals</span></a></li>
+                    <li><a title="Buttons" href="buttons.html"><span class="mini-sub-pro">Buttons</span></a></li>
+                    <li><a title="Tabs" href="tabs.html"><span class="mini-sub-pro">Tabs</span></a></li>
+                    <li><a title="Accordion" href="accordion.html"><span class="mini-sub-pro">Accordion</span></a></li>
+                </ul>
+            </li>
+            <li id="removable">
+                <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-new-file icon-wrap"></i> <span class="mini-click-non">Pages</span></a>
+                <ul class="submenu-angle" aria-expanded="false">
+                    <li><a title="Login" href="login.html"><span class="mini-sub-pro">Login</span></a></li>
+                    <li><a title="Register" href="register.html"><span class="mini-sub-pro">Register</span></a></li>
+                    <li><a title="Lock" href="lock.html"><span class="mini-sub-pro">Lock</span></a></li>
+                    <li><a title="Password Recovery" href="password-recovery.html"><span class="mini-sub-pro">Password Recovery</span></a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
