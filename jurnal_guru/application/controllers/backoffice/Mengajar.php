d1e7c6e5f9dc7790a119568560fc3e79e3fef289
agung wahyu <agungwahyu23699@gmail.com>
2020-05-02 10:33:19 +0700

    Controller mengajar
    Menampilkan form mengajar

--- jurnal_guru/application/controllers/backoffice/Mengajar.php
+++ jurnal_guru/application/controllers/backoffice/Mengajar.php
@@ -14,15 +14,86 @@ class Mengajar extends CI_Controller
 		} else if ($this->uri->segment(3) == "edit" && $_SERVER['REQUEST_METHOD'] == "POST") {
 			$this->update($this->uri->segment(4));
 		}
-
-		$this->load->model("Maksi");
-    }
-    public function index()
-    {
-        $data['title'] = "Data Mengajar";
+	}
+	public function index()
+	{
+		$data['title'] = "Data Mata Mengajar";
 		$data['content'] = "mengajar/indexmengajar";
 		$data['data'] = $this->Maksi->getData("getmengajar");
-		$data['level'] = '1';
 		$this->load->view('backend/index', $data);
-    }
-    public function 
+	}
+	public function add()
+	{
+		$data['title'] = "Tambah Mapel";
+		$data['action'] = "Tambah Data";
+		$data['content'] = "mapel/addmapel";
+		$data['data'] = null;
+		$data['datamapel'] = $this->Maksi->getData('getmapel');
+		$this->load->view('backend/index', $data);
+	}
+
+	public function store()
+	{
+		try {
+
+			$kode = $this->Maksi->random_oke(16);
+
+			$arr = [
+                'kode_mapel' => $kode,
+				'nama_mapel' => $this->input->post('nama_mapel', TRUE),
+				'status' => $this->input->post('status', TRUE),
+				'create_at' => date("Y-m-d H:i:s"),
+				'create_by' => $this->session->userdata('kode_pengguna'),'status'=>1
+			];
+			$this->Maksi->insertData("mapel", $arr);
+			$this->session->set_flashdata("message", ['success', 'Berhasil Menambah Data Mata Pelajaran', ' Berhasil']);
+			redirect(base_url("backoffice/mapel"));
+		} catch (Exception $e) {
+			$this->session->set_flashdata("message", ['danger', 'Gagal Menambah Data Mata Pelajaran', ' Gagal']);
+			$this->add();
+		}
+	}
+
+	public function edit($id)
+	{
+		$data['title'] = "Edit Mata Pelajaran";
+		$data['action'] = "Edit Mata Pelajaran";
+		$data['content'] = "mapel/addmapel";
+		$data['data'] = $this->db->get_where("mapel", ['kode_mapel' => $id])->row_array();
+		$data['datamapel'] = $this->Maksi->getData('getmapel');
+		$this->load->view('backend/index', $data);
+	}
+
+
+	public function update($id)
+	{
+		try {
+			$arr = [
+				'nama_mapel' => $this->input->post('nama_mapel', TRUE),
+			];
+			$this->Maksi->updateData("mapel", $arr, $id, "kode_mapel");
+			$this->session->set_flashdata("message", ['success', 'Berhasil Mengedit Data Mata Pelajaran', ' Berhasil']);
+			redirect(base_url("backoffice/mapel"));
+		} catch (Exception $e) {
+			$this->session->set_flashdata("message", ['danger', 'Gagal Mengedit Data Mata Pelajaran', ' Gagal']);
+			$this->edit($id);
+		}
+	}
+
+	public function delete($id)
+	{
+		try {
+			$arr = [
+				'status' => 0
+			];
+			$this->Maksi->updateData("mapel", $arr, $id, "kode_mapel");
+
+			$this->session->set_flashdata("message", ['success', 'Berhasil Menghapus Data Mata Pelajaran', ' Berhasil']);
+			redirect(base_url("backoffice/mapel"));
+		} catch (Exception $e) {
+			$this->session->set_flashdata("message", ['danger', 'Gagal Menghapus Data Mata Pelajaran', ' Gagal']);
+			redirect(base_url("backoffice/mapel"));
+		}
+	}
+
+}
