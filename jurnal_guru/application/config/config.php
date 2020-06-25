2ce3041a061a8a7b7d243fa81ebe55646cd7c673
ciciliaShelin <cshelin0@gmail.com>
2020-06-25 17:14:41 +0700

    mengajar
    mengajar.php

--- jurnal_guru/application/config/config.php
+++ jurnal_guru/application/config/config.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// $config['base_url'] = 'http://localhost:8080/Workshop_webFramework/jurnal_guru/';
-$config['base_url'] = 'http://localhost/Workshop_webFramework/jurnal_guru/';
-// $config['base_url'] = 'http://guruku.evoindo.com/';
+$config['base_url'] = 'http://localhost:8080/Workshop_webFramework/jurnal_guru/';
+//$config['base_url'] = 'http://localhost/Workshop_webFramework/jurnal_guru/';
+//$config['base_url'] = 'http://guruku.evoindo.com/';
 
 $config['index_page'] = 'index.php';
 
--- jurnal_guru/application/controllers/backoffice/Mengajar.php
+++ jurnal_guru/application/controllers/backoffice/Mengajar.php
@@ -9,20 +9,49 @@ class Mengajar extends CI_Controller
 		if (!$this->session->has_userdata('kode_pengguna') || $this->session->userdata('level')  != '1') {
 			redirect(base_url());
 		}
-		// if ($this->uri->segment(3) == "add" && $_SERVER['REQUEST_METHOD'] == "POST") {
-		// 	$this->store();
-		// } else if ($this->uri->segment(3) == "edit" && $_SERVER['REQUEST_METHOD'] == "POST") {
-		// 	$this->update($this->uri->segment(4));
-		// }
+		if ($this->uri->segment(3) == "add" && $_SERVER['REQUEST_METHOD'] == "POST") {
+			$this->store();
+		} else if ($this->uri->segment(3) == "edit" && $_SERVER['REQUEST_METHOD'] == "POST") {
+			$this->update($this->uri->segment(4));
+		}
 	}
-
 	public function index()
 	{
 		$data['title'] = "Data Mengajar";
 		$data['content'] = "mengajar/indexmengajar";
 		$data['data'] = $this->Maksi->getData("getmengajar");
 		$data['datatahun'] = $this->Maksi->getData("gettahun");
+
 		$this->load->view('backend/index', $data);	
 	}
+	public function store()
+{
+try {
+
+	$kode = $this->Maksi->random_oke(16);
+
+	$arr = [
+		'kode_ijin' => $kodeajar,
+		'kode_guru' => $cek['akses_data'],
+		'create_by' => $cek['kode_pengguna'],
+		'create_at' => date("Y-m-d H:i:s"),
+		'awal' => $this->input->post('awal', TRUE),
+		'akhir' => $this->input->post('akhir', TRUE),
+		'keterangan' => $this->input->post('keterangan', TRUE),
+		'lampiran' => $gambar
+	];
+	$this->Maksi->insertData("ijin_guru", $arr);
+	$data['respon'] = [
+		"pesan" => "Berhasil Membuat Izin. Harap Menunggu Konfirmasi Admin",
+		'status' => true,
+		'code' => 200
+	];
+} else {
+	$data['respon'] = [
+		"pesan" => "Invalid Token",
+		'status' => false,
+		'code' => 403
+	];
 }
-	
+}
+}
