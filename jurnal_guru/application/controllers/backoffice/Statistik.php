866e5bb1b70d0c27efe6b1c60c12b1cc688c1d2e
Gulam Mubarik Ahmad <gulammubarik77@gmail.com>
2020-07-02 00:40:11 +0700

    coba

--- jurnal_guru/application/controllers/backoffice/Statistik.php
+++ jurnal_guru/application/controllers/backoffice/Statistik.php
@@ -20,7 +20,7 @@ class Statistik extends CI_Controller
 		$data['title'] = "Statistik Guru";
 		$data['content'] = "guru/addguru";
         $data['data'] = $this->Maksi->getData("getstatistik");
-        
+		$data['datatahun'] = $this->Maksi->getData("gettahun");
 		$this->load->view('backend/index', $data);	
     }
     public function store()
@@ -31,12 +31,12 @@ class Statistik extends CI_Controller
 
 			$arr = [
                 'kode_guru' => $kode,
-				'kode_tahun' => $this->input->post('tahun_ajaran', TRUE),
+				'kode_tahun' => $this->input->post('tahun_ajaran', TRUE)
 				
 			];
 			$this->Maksi->insertData("guru", $arr);
 			$this->session->set_flashdata("message", ['success', 'Berhasil Menambah Data Guru', ' Berhasil']);
-			redirect(base_url("backoffice/guru/statistik"));
+			redirect(base_url("backoffice/statistik"));
 		} catch (Exception $e) {
 			$this->session->set_flashdata("message", ['danger', 'Gagal Menambah Data Guru', ' Gagal']);
 			$this->add();
